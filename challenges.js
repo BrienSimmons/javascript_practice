858b4891167adccdc7bd6c30baf2a2d959424e0a
MonkeyDYee <brienmsimmons@gmail.com>
2024-04-11 09:04:55 -0500

    new

--- challenges.js
+++ challenges.js
@@ -1,8 +1,20 @@
 /*Print Even Numbers: Write a function that prints all the even numbers up to a given number. 
 Try to make your solution efficient, perhaps by using loops in a creative way that minimizes 
 the amount of code you write.*/
-function printEvenNumbers(limit) {
 
+//input is 10
+//output is [2, 4, 6, 8, 10]
+
+function printEvenNumbers(limit) {
+let evenNumbers = []
+for(let i = 1; i <= limit; i++){
+  let number = i
+  //check to see if number is even. if it is, push it into the array of even numbers
+  if(number % 2 == 0){
+    evenNumbers.push(number)
+  }
+}
+return evenNumbers
 }
 
 /*Multiplication Table: Create a function that prints out a multiplication table up to 10. 
@@ -15,22 +27,48 @@ function printMultiplicationTable() {
 /*Length Converter: Develop a function that converts kilometers to miles.
  The function should take the input in kilometers and return the converted value in miles.*/
  function kilometersToMiles(kilometers) {
-
+  let miles = kilometers/ 1.609
+  return miles
 }
-
+  
 /*Sum of Array: Write a function that calculates the sum of numbers within an array.
  Test your function with different sets of numbers, including both positive and negative numbers,
   as well as integers and decimals.*/
-  function sumArray(numbers) {
 
+  //input: [-1, -2, -3]
+   //output: -6
+
+
+  //input: [-1, 2, -3, 4]
+  //output: -2
+  function sumArray(numbers) {
+    let sum = 0
+  //use a for loop
+  for(let i = 0; i < numbers.length; i++){
+    let number = numbers[i]
+    sum = sum + number // or do this sum += number
+  }
+  return sum
 }
 
 
 /*Reverse Array: Create a function that reverses the order of an array.
  Start with a simple array of 5 numbers, 
  and then test your function with larger arrays to ensure it works correctly.*/
- function reverseArray(arr) {
+ 
+ // input: ["a", "b", "c"]
+// output ["c", "b", "a"]
+// configure a forlooop to iterate backwards over the 
+// array as it iterates, push its value into reverseArray
+  
 
+ function reverseArray(arr) {
+  let reverseArray = []
+  for(let i = arr.length - 1; i >= 0; i--){
+  let value = arr[i]
+  reverseArray.push(value)
+ }
+  return reverseArray
 }
 
 /*Sort Array: Develop a function that sorts an array from lowest to highest. 
